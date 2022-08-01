b672b56ef94e93ffd71fe316d924dc7660e6d1b3
Mateo De Falco <mateodefalco1@gmail.com>
2022-08-01 16:00:59 -0300

    bases INTRO

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import Header from './Componentes/Landing/Header/Header';
-import ScrollBar from './Componentes/UI/ScrollBar/ScrollBar';
-import './App.css';
+import Header from "./Componentes/Landing/Header/Header";
+import Intro from "./Componentes/Landing/Intro/Intro";
+import ScrollBar from "./Componentes/UI/ScrollBar/ScrollBar";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
       <ScrollBar />
+      <Intro />
     </div>
   );
 }
