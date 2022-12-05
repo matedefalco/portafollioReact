52e2d02bd073e54648615242ba6811e23f6e2d06
Mateo De Falco <mateodefalco1@gmail.com>
2022-12-05 13:27:31 -0300

    Título y descripción de cards dinámico

--- src/Componentes/Landing/AboutMe/AboutMe.js
+++ src/Componentes/Landing/AboutMe/AboutMe.js
@@ -7,44 +7,15 @@ const AboutMe = (props) => {
 let texto1 = "Desde 2019 trabajo en el desarrollo web. Me recibí en Digital House como Programador Web Full Stack. Luego de eso, comencé a instruirme en distintos frameworks. Más que nada maquetado de Front End.";
 let texto2 = "Hace +3 años que trabajo e investigo las crtiptomonedas y las tecnologías descentralizadas. He operado con múltiples Dapps en el ecosistema DeFi (Descentralized Finances). Cuento también conexperiencia en NFT´s, interactuando con múltiples coleccionesvirtuales. Soy también Co-Founder de la comunidad CryptoMeetUp, un equipo enfocado en la adopción de las criptomonedas en elmundo.";
 let texto3 = "Trabajo actualmente como Data Analyst en una compañia de producción digital. Utilizando Adobe, Google Analytics, y otrasherramientas, examino las métricas de interacción de los sitios para los mercados de nuestros clientes.";
-
+let titulo = "Título";
   return (
     <Fragment>
       <div className={classes.cajaAbout}>
         <h2>About Me</h2>
         <div className={classes.cajaAboutCards}>
-          <ProfileCard text={texto1}></ProfileCard>
-          <ProfileCard text={texto2}></ProfileCard>
-          <ProfileCard text={texto3}></ProfileCard>
-        </div>
-        <div id="about" className={classes.about}>
-          <div className={classes.aboutItem} id="dev">
-            <p>
-              Desde 2019 trabajo en el desarrollo web. Me recibí en Digital
-              House como Programador Web Full Stack. Luego de eso, comencé a
-              instruirme en distintos frameworks. Más que nada maquetado de
-              Front End.
-            </p>
-          </div>
-          <div className={classes.aboutItem} id="crypto">
-            <p>
-              Hace +3 años que trabajo e investigo las crtiptomonedas y las
-              tecnologías descentralizadas. He operado con múltiples Dapps en el
-              ecosistema DeFi (Descentralized Finances). Cuento también con
-              experiencia en NFT´s, interactuando con múltiples colecciones
-              virtuales. Soy también Co-Founder de la comunidad "CryptoMeetUp",
-              un equipo enfocado en la adopción de las criptomonedas en el
-              mundo.
-            </p>
-          </div>
-          <div className={classes.aboutItem} id="analyst">
-            <p>
-              Trabajo actualmente como Data Analyst en una compañia de
-              producción digital. Utilizando Adobe, Google Analytics, y otras
-              herramientas, examino las métricas de interacción de los sitios
-              para los mercados de nuestros clientes.
-            </p>
-          </div>
+          <ProfileCard titulo ={titulo} text={texto1}></ProfileCard>
+          <ProfileCard titulo ={titulo} text={texto2}></ProfileCard>
+          <ProfileCard titulo ={titulo} text={texto3}></ProfileCard>
         </div>
       </div>
     </Fragment>
--- src/Componentes/UI/ProfileCard/ProfileCard.js
+++ src/Componentes/UI/ProfileCard/ProfileCard.js
@@ -9,7 +9,7 @@ const ProfileCard = (props) => {
             <img src={fotoCard} alt=""></img>
         </div>
         <div className={classes.titulo}>
-            <h1>Titulo</h1>
+            <h1>{props.titulo}</h1>
         </div>
         <div className={classes.descripcion}>
             <p>{props.text}</p>
